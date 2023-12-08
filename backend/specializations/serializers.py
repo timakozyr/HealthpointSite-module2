27c7fed030985cce067b835df7802f1083418777
timakozyr <teprotasov@miem.hse.ru>
2023-12-08 08:29:48 +0300

    Added specializations/ endpoint

---
+++ backend/specializations/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import Specialization
+
+
+class SpecializationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Specialization
+        fields = [
+            "id",
+            "name",
+            "description"
+        ]
--- backend/specializations/views.py
+++ backend/specializations/views.py
@@ -1,4 +1,52 @@
+from django.contrib.auth.models import AnonymousUser
 from django.shortcuts import render
 
+from rest_framework import permissions
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Specialization
+from .serializers import SpecializationSerializer
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        if not isinstance(request.user, AnonymousUser):
+            return request.user.is_admin
+        return False
+
+
+class SpecializationViewSet(ModelViewSet):
+    permission_classes = [IsAdminOrReadOnly]
+    serializer_class = SpecializationSerializer
+    queryset = Specialization.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data,
+                                         partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
