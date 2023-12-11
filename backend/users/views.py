7e3428e9e993ae5eadd50b2ac982f35eb08b30da
timakozyr <teprotasov@miem.hse.ru>
2023-12-11 13:06:22 +0300

    Changed user's queryset

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,35 +2,40 @@ from django.contrib.auth.models import AnonymousUser
 from rest_framework import permissions, status, viewsets
 from rest_framework.response import Response
 
+from roles.models import Role
 from .models import User
 from .serializers import UserSerializer
 
 
-class IsAdminOrReadOnly(permissions.BasePermission):
+class IsDoctorOrAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
+        # if request.method in permissions.SAFE_METHODS:
+        #     return True
 
         if not isinstance(request.user, AnonymousUser):
-            return request.user.is_admin
+            if not bool(request.user and request.user.is_staff):
+                role = Role.objects.get(name="doctor")
+                return request.user.role == role
+            else:
+                return True
         return False
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsDoctorOrAdmin]
 
     def get_queryset(self):
-        if self.request.user.is_admin:
-            return User.objects.all()
+        if self.request.user.is_admin or self.request.user.role.id == 3:
+            return User.objects.filter(role__name="user")
         return User.objects.filter(id=self.request.user.id)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             if (
-                not request.user.is_admin
-                and request.user.id != serializer.validated_data.get("id")
+                    not request.user.is_admin
+                    and request.user.id != serializer.validated_data.get("id")
             ):
                 return Response(
                     {"detail": "You can only create users for yourself."},
